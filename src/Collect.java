125481bd498c483d7dd4154bdcf32c2496684b43
Christian <christiangrissom009@gmail.com>
2020-11-04 16:36:19 -0600

    Working
    working on it

--- src/Collect.java
+++ src/Collect.java
@@ -1,7 +1,35 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.*;
 
 public class Collect {
-//creating a new class
+	HashMap<String, Integer> employees = new HashMap<String, Integer>();
+	TreeMap<String, Integer> PresVotes = new TreeMap<String, Integer>();
+	
 	public Collect() {
 		
 	}
+	public void addEmployee(String firstName, int employeeID) {
+		employees.put(firstName, employeeID);
+	}
+	public void addCandidate(String fullName, int currVotes) {
+		PresVotes.put(fullName, currVotes);
+		
+		
+	}
+	public void turnInVotes(String candName, int newVotes) {
+		if(!PresVotes.containsKey(candName)) {
+			System.out.println("That candidate isn't on the ballot!");
+		}
+		else {
+			int votes = ((int)PresVotes.get(candName));
+			PresVotes.put(candName, (votes + newVotes));
+		}
+	}
+	public void whoIsWinning() {
+		System.out.println(PresVotes);
+	}
+	
+	
 }
